f89e62e20e1f06101cb323c2769f3e80848284f0
Samet Ozan Topcu <sametozan.topcu@gmail.com>
2026-06-29 18:28:11 +0300

    ffnn_baseline.py

--- models/ffnn_baseline.py
+++ models/ffnn_baseline.py
@@ -1,82 +1,68 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
 import tensorflow as tf
-
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
+import numpy as np
+import matplotlib.pyplot as plt
 
 import pathlib
-data_dir = pathlib.Path("/content/drive/MyDrive/colab projects/SMILEs")
-data_dir
+dataset_url ="C:/Users/ibrah/PycharmProjects/finger_tracking/SMILEs"
+data_dir = pathlib.Path(dataset_url)
+
 
 batch_size = 50
-image_height = 64
-image_width = 64
+seq_length = 64
+input_size = 64
+epochs = 10
+num_classes = 2
 
-
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(data_dir,validation_split=0.2,subset='training',seed=123,image_size=(image_height,image_width), batch_size=batch_size)
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  dataset_url,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(input_size, seq_length),
+  batch_size=batch_size)
 
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
   subset="validation",
   seed=123,
-  image_size=(image_height, image_width),
+  image_size=(input_size, seq_length),
   batch_size=batch_size)
 
-class_names = train_ds.class_names
-print(class_names)
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
 
-AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+normalized_train_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+normalized_val_ds = val_ds.map(lambda x, y: (normalization_layer(x), y))
 
-model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1./255, input_shape=(image_height,image_width,3)),
-    layers.Conv2D(16,3,padding='same',activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes)])
-
-model.compile(optimizer='adam',
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(input_size, seq_length,3)),
+    tf.keras.layers.Dense(2, activation='sigmoid')
+])
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer= optimizer,
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-
+model.build()
 model.summary()
 
-for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
+history = model.fit(normalized_train_ds, validation_data=normalized_val_ds, epochs=epochs)
 
-num_epochs = 5
-history = model.fit(train_ds, 
-                    validation_data = val_ds,
-                    num_epochs = num_epochs)
-
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(epochs)
-
-plt.figure(figsize=(7, 7))
+# Plot training loss and accuracy
+plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
 
 plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
 plt.show()
